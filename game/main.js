311aa4c0b7f348a532ecf8df4f5641e438558182
huvalk <41309072+huvalk@users.noreply.github.com>
2018-07-20 11:02:28 +0300

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,19 +1,24 @@
 "use strict";
 
 window.onload = function() {
-	let playBtn = document.getElementById("playBtn");
-	let resultBtn = document.getElementById("resultBtn");
-	let aboutBtn = document.getElementById("aboutBtn");
-	
-	playBtn.onclick	= function() {
-		window.location = "play.html";
-	}
-	
-	resultBtn.onclick = function() {
-		window.location = "results.html";
-	}
-	
-	aboutBtn.onclick = function() {
-		window.location = "about.html";
-	}
+  let playBtn = document.getElementById("playBtn");
+  let resultsBtn = document.getElementById("resultsBtn");
+  let aboutBtn = document.getElementById("aboutBtn");
+  let delBtn = document.getElementById ("delBtn");
+  
+  playBtn.onclick = function () {
+      window.location = "play.html";
+  };
+
+  resultsBtn.onclick = function () {
+      window.location = "results.html";
+  };
+
+  aboutBtn.onclick = function () {
+      window.location = "about.html";
+  };
+  
+  delBtn.onclick = function () {
+		localStorage.clear ();
+  }
 };
